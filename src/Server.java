2b6cb9bad6bc7c98ab68ebd03f73b8ad61ceecb0
DanielaSMR <10828775@ieselgrao.org>
2024-12-17 00:39:47 +0100

    1.2v

--- src/Server.java
+++ src/Server.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 class Server implements Runnable{
     private ServerSocket server = null;
@@ -15,6 +17,8 @@ class Server implements Runnable{
     private BufferedReader bf = null;
     private PrintWriter pw = null;
     private OutputStream os = null;
+    private List<Ticket> tickets = new ArrayList<>();
+
     int port = 0;
 
     public Server(int port){
@@ -23,17 +27,15 @@ class Server implements Runnable{
 
     @Override
     public void run(){
-        
-
         System.out.println("INFO: Server launching...");
-
+        boolean seguirPrograma = true;
         try {
             server = new ServerSocket(port);
         } catch (Exception e) {
             System.out.println("ERROR: Unable to open socket on TCP " + port );
             return;
         }
-        while (true) {
+        while (seguirPrograma) {
             try {
                 cliente = server.accept();
                 System.out.println("OK: Connection");
@@ -42,25 +44,89 @@ class Server implements Runnable{
                 isr = new InputStreamReader(is);
                 bf = new BufferedReader(isr);
 
+                os = cliente.getOutputStream();
+                pw = new PrintWriter(os);
+
                 //Leer mensajes
-                System.out.println("SERVER: Waiting");
-                String nombre = bf.readLine();
-                System.out.println("SERVER: Message received");
-                recibirNombre(nombre);
+                    try {
+                        //Recibir nombre
+                        System.out.println("SERVER: Waiting");
+                        pw.write("Por favor, introduce tu nombre:\n");
+                        pw.flush();
+                        System.out.println("SERVER: Nombre pedido...Esperando respuesta");
 
-                String problema = bf.readLine();
+                        String nombre = bf.readLine();
+                        recibirNombre(nombre);
+                        System.out.println("SERVER: Message received");
 
-               
+                        //Recibir Problema
+                        System.out.println("SERVER: Waiting");
+                        pw.write("Por favor, introduce el problema:\n");
+                        pw.flush();
+                        System.out.println("SERVER: Problema pedido...Esperando respuesta");
 
-                //Cerrar
-                pw.close();
-                os.close();
-                bf.close();
-                isr.close();
-                is.close();
-                cliente.close();
+                        String problema = bf.readLine();
+
+                        while (problema.length() > 50) {
+                            System.out.println("El problema es demasiado largo...\nEscribelo de nuevo");
+                            problema = bf.readLine();
+                        }
+                        System.out.println("SERVER: Message received");
+                        recibirProblema(problema);
+
+                        //Recibir Prioridad
+                        System.out.println("SERVER: Waiting");
+                        pw.write("Por favor, introduce la prioridad del problema(1-5):\n");
+                        pw.flush();
+                        System.out.println("SERVER: Nombre pedido...Esperando respuesta");
+
+                        String prioridadS = bf.readLine();
+                        int numPrioridad = Integer.parseInt(prioridadS);
+                        System.out.println("SERVER: Message received");
+                        recibirPrioridad(numPrioridad);
+
+                        //Recibir Estado
+                        System.out.println("SERVER: Waiting");
+                        pw.write("Por favor, introduce el estado del problema:\n 1-No solucionado \n 2-En proceso \n 3-Solucionado \n");
+                        pw.flush();
+                        System.out.println("SERVER: Nombre pedido...Esperando respuesta");
+
+                        String estadoS = bf.readLine();
+                        int numEstado = Integer.parseInt(estadoS);
+                        System.out.println("SERVER: Message received");
+                        recibirEstado(numEstado);
+
+                        //Crear y guardar en una lista
+                        Ticket ticket = new Ticket(nombre, problema, numPrioridad,numEstado);
+                        tickets.add(ticket);
+
+                        // Confirmar al usuario
+                        pw.write("Ticket creado exitosamente: " + ticket + "\n");
+                        pw.flush();
+
+                        //Mostrar Ticket
+                        System.out.println(ticket.toString());
+
+                        //Terminando..
+                        System.out.println("Que le gustaria hacer? \n 1-Crear otro ticket \n 2-Mostrar tickets creados \n 3-Irse");
+                        String finalS = bf.readLine();
+                        int numFinal = Integer.parseInt(finalS);
+                        if(numFinal == 2){
+                            mostrarTickets();
+                        }else if(numFinal == 3){
+                            cerrarConexion();
+                            seguirPrograma = false;
+                        }
+
+                        //Cerrar
+                        cerrarConexion();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println(e.getMessage());
+                cerrarConexion();
+                return;
             }
             
 
@@ -68,37 +134,93 @@ class Server implements Runnable{
         }
     }
 
-    public void recibirNombre(String nombre){
+    public void recibirNombre(String nombre) throws Exception {
+
+        if (nombre.length() > 20) {
+            throw new Exception("ERROR: Nombre demasiado largo");
+        }else{
+            System.out.println("SERVER: Nombre recibido");
+        }
+
         try {
             String answer = "Hola " + nombre + " ! :)\n Vamos a crear tu ticket";
-            os = cliente.getOutputStream();
-            pw = new PrintWriter(os);
             pw.write(answer);
             pw.flush();
-            System.out.println("SERVER: Message name sent");
+            System.out.println("SERVER: Mensaje enviado");
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("ERROR: problema al recibir el nombre");
         }
-        
     }
 
-    public void recibirProblema(String problema){
-        if(problema.length() > 50){
-            
-        }
+    public void recibirProblema(String problema) throws Exception{
         try {
-            String answer = "Hola " + nombre + " ! :)\n Vamos a crear tu ticket";
-            os = cliente.getOutputStream();
-            pw = new PrintWriter(os);
+            String answer = "Problema recibido! Pasemos al siguiente dato";
             pw.write(answer);
             pw.flush();
-            System.out.println("SERVER: Message name sent");
+            System.out.println("SERVER: Message problem sent");
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("ERROR: problema al recibir el nombre");
+            System.out.println("ERROR: problema al recibir el problema");
         }
         
     }
 
+    public void recibirPrioridad(int prioridad) throws Exception{
+        if(prioridad > 5){
+            prioridad = 5;
+        }else if(prioridad < 1){
+            prioridad = 1;
+        }
+        try {
+            String answer = "Prioridad recibida! Pasemos al siguiente dato";
+            pw.write(answer);
+            pw.flush();
+            System.out.println("SERVER: Message priority sent");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ERROR: problema al recibir la prioridad");
+        }
+        
+    }
+
+    public void recibirEstado(int estado) throws Exception{
+        if(estado > 3 || estado < 1){
+            throw new Exception("ERROR: No se selecciono una opción");
+        }else{
+            System.out.println("SERVER: Estado recibido");
+        }
+        try {
+            String answer = "Estado recibido! Pasemos al final";
+            pw.write(answer);
+            pw.flush();
+            System.out.println("SERVER: Message sent");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ERROR: problema al recibir el estado");
+        }
+        
+    }
+
+    public void mostrarTickets() {
+        System.out.println( "----- Lista de Tickets -----");
+        for (Ticket ticket : tickets) {
+            System.out.println(ticket);
+        }
+        System.out.println("----------------------------");
+    }
+
+    private void cerrarConexion() {
+        try {
+            if (pw != null) pw.close();
+            if (os != null) os.close();
+            if (bf != null) bf.close();
+            if (isr != null) isr.close();
+            if (is != null) is.close();
+            if (cliente != null) cliente.close();
+            if (server != null) server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/Ticket.java
@@ -0,0 +1,69 @@
+public class Ticket {
+    public static final String RESET = "\033[0m";  // Text Reset
+    public static final String RED = "\033[0;31m";    // RED
+    public static final String GREEN = "\033[0;32m";  // GREEN
+    public static final String YELLOW = "\033[0;33m"; // YELLOW
+
+    private String nombre;
+    private String problema;
+    private int prioridad;
+    private int estado;
+
+    public Ticket(String nombre, String problema, int prioridad,int estado) {
+        this.nombre = nombre;
+        this.problema = problema;
+        this.prioridad = prioridad;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        String color = "";
+        String estadoS = "";
+        if(estado == 1){
+            color = RED;
+            estadoS = "No solucionado";
+        }else if(estado == 2){
+            color = YELLOW;
+            estadoS = "En proceso";
+        }else if(estado == 3){
+            color = GREEN;
+            estadoS = "Solucionado";
+        }
+        return "Ticket{" + color + "•" + RESET + problema + "\n Usuario: " + nombre + "\r Prioridad: " + prioridad + "\r Estado: " + color + estadoS + RESET +'}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProblema() {
+        return problema;
+    }
+
+    public void setProblema(String problema) {
+        this.problema = problema;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    
+}

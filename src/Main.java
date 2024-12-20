4af40888680f69b1add7ab53ceb13a1170a77803
DanielaSMR <10828775@ieselgrao.org>
2024-12-20 15:24:21 +0100

    1.5v

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int port = 12345; // Puerto que usará el servidor y el cliente
-        String host = "localhost"; // Dirección del host
+        int port = 12345; 
+        String host = "localhost";
 
-        // Iniciar el servidor en un hilo separado
         Server2 server = new Server2(port);
         Thread serverThread = new Thread(server);
         serverThread.start();
 
-        // Iniciar el cliente en el hilo principal
         new Cliente2(host, port);
     }
 }
